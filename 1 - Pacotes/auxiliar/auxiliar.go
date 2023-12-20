117e62385d509c8119f20a34784457625f79f34c
leandro-miranda <leandromiranda2010@gmail.com>
2023-12-20 10:30:41 -0300

    criando pacotes para interagirem entre si

---
+++ 1 - Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+// Escrever é uma função responsável por escrever uma mensagem na tela
+func Escrever() {
+		fmt.Println("Escrevendo do arquivo auxiliar")
+		escrever2()
+}
---
+++ 1 - Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+// Escrever2 é uma função responsável por escrever uma mensagem na tela
+func escrever2() {
+		fmt.Println("Escrevendo pela função escrever2")
+}
