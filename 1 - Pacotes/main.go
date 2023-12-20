51cd8c89e630bebd5dec11445f62c9513d4d6940
leandro-miranda <leandromiranda2010@gmail.com>
2023-12-20 10:43:11 -0300

    instalando dependencia externa

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,16 @@ package main
 import (
 		"fmt"
 		"modulo/auxiliar"
+		"github.com/badoux/checkmail"
 )
 
 func main(){
 		fmt.Println("Escrevendo do arquivo main")
 		auxiliar.Escrever()
+
+		erro := checkmail.ValidateFormat("devbook@gmail.com")
+		fmt.Println(erro)
+
+		erro2 := checkmail.ValidateFormat("123")
+		fmt.Println(erro2)
 }
