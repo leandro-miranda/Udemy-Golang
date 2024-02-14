073488e32942b8649326be005ab3ee63c8796ddd
leandro-miranda <leandromiranda2010@gmail.com>
2024-02-14 17:33:28 -0300

    funções

---
+++ 4 - Funções/funcoes.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
+func main() {
+	soma := somar(10, 20)
+	fmt.Println(soma)
+
+	var f = func(txt string) string {
+		fmt.Println(txt)
+		return txt
+	}
+
+	resultado := f("Texto da função 1")
+	fmt.Println(resultado)
+
+	// resultadoSoma, resultadoSubtracao := calculosMatematicos(10, 15)
+	// fmt.Println(resultadoSoma, resultadoSubtracao)
+
+	// resultadoSoma, _ := calculosMatematicos(10, 15) // Ignorando o segundo retorno da função com _(underline)
+	// fmt.Println(resultadoSoma)
+
+	_, resultadoSubtracao := calculosMatematicos(10, 15) // Ignorando o segundo retorno da função com _(underline)
+	fmt.Println(resultadoSubtracao)
+}
